a55b7041dc7001a1dbe643aaf9813a2f8f065bde
marah-jaradat <marahjaradat97@gmail.com>
2022-02-14 08:51:40 +0200

    first push'

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <a href="url">FaceBook</a>
+      <a href="url">Instagam</a>
+      <p>The auther: Marah Jaradat</p>
+    </>
+  );
+}
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Tours from "./components/Tours";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Tours />
+    </>
+  );
+}
