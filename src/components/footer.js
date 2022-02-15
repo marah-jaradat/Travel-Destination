14234925d50357845deb6ccc2bc3d393f036f10f
marah-jaradat <marahjaradat97@gmail.com>
2022-02-15 08:54:56 +0200

    first push

--- src/components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <>
-      <p>The auther: Marah Jaradat</p>
-      <a href="url">FaceBook</a>
-      <a href="url">Instagam</a>
-    </>
-  );
-}
---
+++ src/components/tours/tour/tour.js
@@ -0,0 +1,11 @@
+import "./tour.css";
+
+export default function tour(props) {
+  return (
+    <div>
+      <p id="name">{props.data3.name}</p>
+      <img src={props.data3.image} />
+      <hr></hr>
+    </div>
+  );
+}
